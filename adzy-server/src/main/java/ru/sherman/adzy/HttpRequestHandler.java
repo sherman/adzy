803e98258e1bc5931edc0ee31fce5ed4f3487d55
Denis M. Gabaidulin <gabaden@gmail.com>
2011-10-16 22:29:52 +0400

    + trivia http request handler

--- adzy-server/src/main/java/ru/sherman/adzy/HttpRequestHandler.java
+++ adzy-server/src/main/java/ru/sherman/adzy/HttpRequestHandler.java
@@ -1,7 +1,17 @@
 package ru.sherman.adzy;
 
-import org.jboss.netty.channel.ChannelHandler;
-import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.apache.log4j.Logger;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.handler.codec.http.*;
+import org.jboss.netty.util.CharsetUtil;
+
+import java.util.Set;
+
+import static org.jboss.netty.handler.codec.http.HttpHeaders.*;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.*;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.*;
+import static org.jboss.netty.handler.codec.http.HttpVersion.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,5 +21,59 @@ import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
  * To change this template use File | Settings | File Templates.
  */
 public class HttpRequestHandler extends SimpleChannelUpstreamHandler {
+    private static final Logger log = Logger.getLogger(HttpRequestHandler.class);
 
+    private HttpRequest request;
+    private final StringBuilder out = new StringBuilder();
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        log.debug("Msg received");
+        HttpRequest request = this.request = (HttpRequest) e.getMessage();
+        // do work here
+        out.append("\r\n");
+    }
+
+    public void writeResponse(MessageEvent e) {
+        // Decide whether to close the connection or not.
+        boolean keepAlive = isKeepAlive(request);
+
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+        response.setContent(ChannelBuffers.copiedBuffer(out.toString(), CharsetUtil.UTF_8));
+        response.setHeader(CONTENT_TYPE, "text/plain; charset=UTF-8");
+
+        if (keepAlive) {
+            // Add 'Content-Length' header only for a keep-alive connection.
+            response.setHeader(CONTENT_LENGTH, response.getContent().readableBytes());
+        }
+
+        // Encode the cookie.
+        String cookieString = request.getHeader(COOKIE);
+        if (cookieString != null) {
+            CookieDecoder cookieDecoder = new CookieDecoder();
+            Set<Cookie> cookies = cookieDecoder.decode(cookieString);
+
+            if (!cookies.isEmpty()) {
+                // Reset the cookies if necessary.
+                CookieEncoder cookieEncoder = new CookieEncoder(true);
+                for (Cookie cookie : cookies) {
+                    cookieEncoder.addCookie(cookie);
+                }
+                response.addHeader(SET_COOKIE, cookieEncoder.encode());
+            }
+        }
+
+        // Write the response.
+        ChannelFuture future = e.getChannel().write(response);
+        // Close the non-keep-alive connection after the write operation is done.
+        if (!keepAlive) {
+            future.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        log.error("Exception occured in http handler", e.getCause());
+        e.getChannel().close();
+    }
 }
