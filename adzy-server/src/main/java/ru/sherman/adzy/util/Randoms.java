233e00adb63e8ef8988947d723709ae3118d43e4
Denis M. Gabaidulin <gabaden@gmail.com>
2011-10-16 23:29:53 +0400

    + random interval generator

---
+++ adzy-server/src/main/java/ru/sherman/adzy/util/Randoms.java
@@ -0,0 +1,25 @@
+package ru.sherman.adzy.util;
+
+import java.util.Random;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sherman
+ * Date: 16.10.11
+ * Time: 23:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Randoms {
+    private Randoms() {}
+
+    public Interval<Integer> getRandomInterval(Integer max) {
+        Random rand = new Random();
+
+        int randomValue = 0;
+        do {
+            randomValue = rand.nextInt(max);
+        } while (randomValue == 0);
+
+        return new Interval<Integer>(randomValue, randomValue);
+    }
+}
